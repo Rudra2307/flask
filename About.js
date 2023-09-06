619d0de285919b8783dfc5a1c10a17a8aed7f771
Rudra2307 <67470013+Rudra2307@users.noreply.github.com>
2023-09-06 23:37:39 +0530

    o;w;odw

--- About.js
+++ About.js
@@ -2,15 +2,16 @@ import React from 'react'
 function signup(){
     console.log("hi");
 }
-function About() {
-  return (
-    <div>
-      <p>Hey this is  Dhruv Bheda</p>
-      <br />
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem veniam in asperiores esse officiis, minima cum aliquam ipsa tempora commodi consequuntur nesciunt culpa necessitatibus illum, facere odio deleniti nemo magni?</p>
-      <button onClick={signup}>Sign Up</button>
-    </div>
-  )
+class About extends React.Component {
+    render() {
+        return (
+
+            <div>
+                <button onClick={signup}>Sign Up</button>
+               
+            </div>
+        )
+    }   
 }
 
 export default About
