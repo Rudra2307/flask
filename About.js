48923e23d7b3fb43f251483355431b4ff9f64c8d
DhruvB123 <dhruvbheda009@gmail.com>
2023-09-06 23:12:10 +0530

    Added about.js

---
+++ About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+      <p>Hey this is about page by person 2 </p>
+      <br />
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem veniam in asperiores esse officiis, minima cum aliquam ipsa tempora commodi consequuntur nesciunt culpa necessitatibus illum, facere odio deleniti nemo magni?</p>
+    </div>
+  )
+}
+
+export default About
